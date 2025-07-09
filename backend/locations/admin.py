71efe9adafb8cb943aec682d6d3352f95ee40a22
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-09 09:36:36 +0330

    Add admin interfaces for Country, City, and Address models with display and filter options

--- backend/locations/admin.py
+++ backend/locations/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import BaseAdmin
+from locations.models import Country, City, Address
+
+
+@admin.register(Country)
+class CountryAdmin(BaseAdmin):
+    """
+    Admin interface for managing countries.
+    """
+    list_display = ['name', 'created_at', 'updated_at']
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(City)
+class CityAdmin(BaseAdmin):
+    """
+    Admin interface for managing cities.
+    """
+    list_display = ['name', 'country', 'created_at', 'updated_at']
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_filter = ('country',)
+
+
+@admin.register(Address)
+class AddressAdmin(BaseAdmin):
+    """
+    Admin interface for managing addresses.
+    """
+    list_display = ['user',
+                    'store',
+                    'city',
+                    'address',
+                    'postal_code',
+                    'state',
+                    'phone',
+                    'is_default',
+                    'created_at',
+                    'updated_at']
+    search_fields = ('address', 'postal_code', 'state')
+    ordering = ('city__name',)
+    list_filter = ['city', 'is_default']
+    
