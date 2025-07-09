192bcfdca855fc3ff00e68c87f44e713cbfd438a
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-09 11:15:52 +0330

    Add custom User model and UserManager with role choices and email as username

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as DjangoUserManager
 
-# Create your models here.
+from base.models import BaseModelQuerySet, BaseModel
+
+
+class UserManager(DjangoUserManager.from_queryset(BaseModelQuerySet)):
+    """
+    Custom UserManager that extends Django's UserManager with additional queryset methods.
+    """
+    pass
+
+
+class User(AbstractUser, BaseModel):
+    """
+    Custom User model that extends Django's AbstractUser.
+    """
+    USER_ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+        ('seller', 'Seller'),
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    role = models.CharField(max_length=10, choices=USER_ROLE_CHOICES, default='customer')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone']
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.email
