17d97932c4081a57f1bab950169b77afea651c96
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-09 11:20:04 +0330

    Organize INSTALLED_APPS and set custom User model in settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-party apps
     "rest_framework",
-    
+    # local apps
+    "accounts",
+    "discounts",
+    "locations",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +135,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
