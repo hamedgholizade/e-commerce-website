431ac2a1edd3d3d61aa225922c919a438d2ed4c1
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-10 20:47:54 +0330

    Add orders app to settings and implement admin interfaces for Order and OrderItem models

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import BaseAdmin
+from orders.models import Order, OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(BaseAdmin):
+    """
+    Admin interface for Order model.
+    """
+    list_display = ['user__phone',
+                    'discount__name',
+                    'status', 
+                    'total_price',
+                    'created_at',
+                    'updated_at'
+                    ]
+    search_fields = ['user__phone',
+                     'user__first_name',
+                     'user__last_name',
+                     'user__email',
+                    'status',
+                    ]
+    list_filter = BaseAdmin.list_filter + [
+        'user__phone',
+        'user__email',
+        'discount__code',
+        'discount__name',
+        'status'
+        ]
+    
+
+@admin.register(OrderItem)
+class OrderItemAdmin(BaseAdmin):
+    """
+    Admin interface for OrderItem model.
+    """
+    list_display = ['order__id',
+                    'store_item__product__name',
+                    'quantity',
+                    'price_at_purchase',
+                    'created_at',
+                    'updated_at'
+                    ]
+    search_fields = ['order__id',
+                     'store_item__product__name',
+                     'order__user__phone',
+                    ]
+    list_filter = BaseAdmin.list_filter + [
+        'order__status',
+        'store_item__product__name',
+        'order__user__phone',
+        ]
