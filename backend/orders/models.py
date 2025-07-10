2ea8f95714f080bf9f9c1bbd1b2897e7c5a15a38
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-10 20:37:31 +0330

    Add Order and OrderItem models with related fields and methods

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from base.models import BaseModel
+from locations.models import Address
+from discounts.models import Discount
+from stores.models import StoreItem
+
+User = get_user_model()
+
+
+class Order(BaseModel):
+    """
+    Model representing an order in the e-commerce system.
+    """
+    ORDER_STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('processing', 'Processing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+        ('returned', 'Returned'),
+        ('refunded', 'Refunded'),
+        ('failed', 'Failed'),
+    ]
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders'
+        )
+    address = models.ForeignKey(
+        Address, on_delete=models.CASCADE, related_name='orders'
+        )
+    discount = models.ForeignKey(
+        Discount, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders'
+        )
+    status = models.CharField(
+        max_length=20, choices=ORDER_STATUS_CHOICES, default='pending'
+        )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        """
+        Override save method to ensure total_price is calculated before saving.
+        """
+        if not self.total_price:
+            self.total_price = sum(item.price_at_purchase * item.quantity for item in self.items.all())
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"""
+                Order {self.id} by {self.user.phone}/
+                {self.user.first_name}.{self.user.last_name}
+                - Status: {self.status}"""
+
+
+class OrderItem(BaseModel):
+    """
+    Model representing an item in an order.
+    """
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='items'
+        )
+    store_item = models.ForeignKey(
+        StoreItem, on_delete=models.CASCADE, related_name='order_items'
+        )
+    quantity = models.PositiveIntegerField(default=0)
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"""
+                Item {self.id} in Order 
+                {self.order.id} - {self.store_item.product.name}
+                x {self.quantity}"""
