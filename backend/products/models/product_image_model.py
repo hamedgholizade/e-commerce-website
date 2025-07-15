ba56a604c11085d60355332c9d6eb4fcb128be30
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-16 00:13:45 +0330

    refactor Product model and ProductImage model with nw final-project and ERD

--- backend/products/models/product_image_model.py
+++ backend/products/models/product_image_model.py
@@ -11,9 +11,6 @@ class ProductImage(BaseModel):
     """
     product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='media/products' ,default='media/products/default.jpg')
-    alt_text = models.CharField(max_length=255, blank=True, null=True)
-    is_primary = models.BooleanField(default=False)
-
     def __str__(self):
-        return f"image for {self.product.title}"
+        return f"image for {self.product.name}"
  
--- backend/products/models/product_model.py
+++ backend/products/models/product_model.py
@@ -9,30 +9,16 @@ class Product(BaseModel):
     """
     Model to store product information.
     """
-    PRODUCT_STATUS_CHOICES = [
-        ('active', 'Active'),
-        ('inactive', 'Inactive'),
-        ('draft', 'Draft'),
-    ]
-    title = models.CharField(max_length=255, unique=True)
+    
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    slug = models.SlugField(unique=True, null=True, blank=True)
-    brand = models.CharField(max_length=100)
-    status = models.CharField(
-        max_length=8,
-        choices=PRODUCT_STATUS_CHOICES,
-        default='draft',
-    )
+    stock = models.CharField(max_length=100, blank=True, null=True)
+    rating = models.CharField(max_length=50, blank=True, null=True)
     category = models.ManyToManyField(
         Category,
         related_name='products'
     )
 
-    def save(self, *args, **kwargs):
-        """Override save method to auto-generate slug from title."""
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
-
     def __str__(self):
-        return self.title
+        return self.name
 
