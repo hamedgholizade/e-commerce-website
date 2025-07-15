b85804da73cec7bbd28fdde7cdf54836a7aee2d4
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-16 02:16:15 +0330

    create models.py and admin.py for reviews-app with fields of final-project and inherite from base app

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import BaseAdmin
+from reviews.models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(BaseAdmin):
+    """
+    Admin interface for Review model.
+    """
+    list_display = ['user__phone',
+                    'product__name',
+                    'store__name',
+                    'created_at',
+                    'updated_at'
+                    ]
+    search_fields = ['user__phone',
+                     'product__name',
+                     'store__name'
+                     ]
+    list_filter = BaseAdmin.list_filter + ['user__phone', 'product', 'store']
--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from base.models import BaseModel
+from stores.models import Store
+from products.models import Product
+
+User = get_user_model()
+
+
+class Review(BaseModel):
+    """
+    Model representing a product review.
+    """
+    
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        null=True,
+        blank=True
+    )
+    store = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        null=True,
+        blank=True
+    )
+    parent_review = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='replies',
+        blank=True,
+        null=True
+    )
+    comment = models.TextField()
+    
+    def clean(self):
+        if not self.product and not self.store:
+            raise ValidationError("At least one of product or store must be set.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Ensure validation is run before saving
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Review by {self.user.phone} for {self.product.name}/{self.store.name}"
