a67fe8aa4048cc54a783dcf470c7360a609d36d6
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-16 02:00:59 +0330

    create and write Cart models and admin , CartItem models and admin with ERD

--- backend/carts/admin.py
+++ backend/carts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import BaseAdmin
+from carts.models import Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(BaseAdmin):
+    """
+    Admin interface for Cart model.
+    """
+    list_display = ['user__phone',
+                    'total_price',
+                    'created_at',
+                    'updated_at'
+                    ]
+    search_fields = ['user__phone',
+                     'user__email'
+                    ]
+    list_filter = BaseAdmin.list_filter + ['user']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(BaseAdmin):
+    """
+    Admin interface for CartItem model.
+    """
+    list_display = ['cart__user__phone',
+                    'store_item__product__name',
+                    'quantity',
+                    'created_at',
+                    'updated_at'
+                    ]
+    search_fields = ['cart__user__phone',
+                     'store_item__product__name'
+                     ]
+    list_filter = BaseAdmin.list_filter + ['cart', 'store_item']
--- backend/carts/models.py
+++ backend/carts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from base.models import BaseModel
+from stores.models import StoreItem
+
+User = get_user_model()
+
+
+class Cart(BaseModel):
+    """
+    Model representing a shopping cart.
+    """
+    
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='carts'
+    )
+    total_price = models.CharField(
+        max_length=100, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"Cart of {self.user.phone}"
+
+
+class CartItem(BaseModel):
+    """
+    Model representing an item in a shopping cart.
+    """
+    
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    store_item = models.ForeignKey(
+        StoreItem,
+        on_delete=models.CASCADE,
+        related_name='cart_items'
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.store_item.product.name} in {self.cart.user.phone}'s cart"
