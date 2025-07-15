4fc44b987c81fbc6e5387bc27523c267857b28a5
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-16 00:02:52 +0330

    refactor category model with new project final and put default photo for category

--- backend/products/models/category_model.py
+++ backend/products/models/category_model.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.text import slugify
 
 from base.models import BaseModel
 
@@ -9,19 +8,16 @@ class Category(BaseModel):
     Model to store product categories.
     """
     name = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(unique=True, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to='media/categories/', default='media/categories/default.jpg'
+    )
     parent = models.ForeignKey(
         'self', related_name='subcategories', on_delete=models.CASCADE, blank=True, null=True
     )
     
     class Meta:
         ordering = ['name']
-        
-    def save(self, *args, **kwargs):
-        """Override save method to auto-generate slug from name."""
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.name
