92b687688c29530cf7633687cac774a6651649c1
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-17 11:04:49 +0330

    create accounts/serializers.py and make LoginSerializer and RegisterSerializer in it

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'phone',
+            'email',
+            'password',
+            'first_name',
+            'last_name',
+            'is_seller',
+            'picture'
+            ]
+
+    def create(self, validated_data):
+        phone = validated_data.get("phone")
+        validated_data['username'] = phone
+        return super().create(validated_data)
+    
+
+class LoginSerializer(serializers.Serializer):
+    phone_or_email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
