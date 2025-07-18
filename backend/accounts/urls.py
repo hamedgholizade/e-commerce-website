cfd7fda69b7794043e3a62688f81469a3a91ca73
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-18 12:08:08 +0330

    add pathes 'logout/' and 'token/verify/' for LogoutAPIView and VerifyTokenAPIView

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,13 +3,17 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views import (
     RegisterAPIView,
-    LoginAPIView
+    LoginAPIView,
+    LogoutAPIView,
+    VerifyTokenAPIView
 )
 
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='accounts-register'),
     path('login/', LoginAPIView.as_view(), name='login-token'),
+    path('logout/', LogoutAPIView.as_view(), name='logout-token'),
     path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
+    path('token/verify/', VerifyTokenAPIView.as_view(), name='verify-token'),
     
 ]
