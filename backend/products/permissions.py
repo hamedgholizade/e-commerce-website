abc70e0475336f6fd3c6b2f015006107ced40a32
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-22 17:56:35 +0330

    create products/permissions.py and write class IsAdminOrSellerOrReadOnly permission for when user is seller or admin can PUT, PATCH, DELETE, POST else just can GET operation in it.

---
+++ backend/products/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import (
+    BasePermission,
+    SAFE_METHODS
+)
+
+
+class IsAdminOrSellerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method not in SAFE_METHODS:
+            if not request.user.is_authenticated:
+                return False 
+            return bool(
+                request.user.is_seller or
+                request.user.is_staff or
+                request.user.is_superuser
+            )
+        return True
