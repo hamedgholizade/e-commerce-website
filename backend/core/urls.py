23b14396a8092f2d7b7bd4fee58140cb81893304
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-22 17:48:36 +0330

    add pathes in core/urls.py for locations app and products app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
+    path('api/locations/', include("locations.urls")),
+    path('api/products/', include('products.urls'))
 ]
 
 if settings.DEBUG:
