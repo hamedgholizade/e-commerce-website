71b7f2c04992be469bc4f7e9d7b39527f9c17168
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-07-25 12:02:23 +0330

    in carts/models.py create total_price method in Cart class model for getting total price of all cart items, create total_price (for getting unit_price * quantity of item that customer added) and unit_price (for getting price of product that user add this to cart) methods in Cartitem class model.

--- backend/carts/models.py
+++ backend/carts/models.py
@@ -20,6 +20,11 @@ class Cart(BaseModel):
     total_price = models.CharField(
         max_length=100, blank=True, null=True
     )
+    
+    @property
+    def total_price(self):
+        self.total_price = sum(self.items.total_price())
+        return self.total_price
 
     def __str__(self):
         return f"Cart of {self.user.phone}"
@@ -42,5 +47,15 @@ class CartItem(BaseModel):
     )
     quantity = models.PositiveIntegerField(default=1)
 
+    @property
+    def unit_price(self):
+        if self.store_item.discount_price:
+            return self.store_item.discount_price
+        return self.store_item.price
+    
+    @property
+    def total_price(self):
+        return float(self.unit_price) * self.quantity
+    
     def __str__(self):
         return f"{self.store_item.product.name} in {self.cart.user.phone}'s cart"
