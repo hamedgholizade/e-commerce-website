daa7d4feb0ec77aa1664ba2a21f4ccd6afa3f51d
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-01 13:58:15 +0330

    fix some issues in carts/serializers.py and in CartItemSerializer override func create and refactor CartSerializer class fields

--- backend/carts/serializers.py
+++ backend/carts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from accounts.serializers import UserViewProfileSerializer
+from stores.serializers import StoreItemSerializer
 from carts.models import (
     Cart,
     CartItem
@@ -8,6 +8,7 @@ from carts.models import (
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    store_item_data = StoreItemSerializer(read_only=True)
     unit_price = serializers.FloatField(read_only=True)
     total_price = serializers.FloatField(read_only=True)
     
@@ -19,15 +20,51 @@ class CartItemSerializer(serializers.ModelSerializer):
             'store_item',
             'quantity',
             'unit_price',
-            'total_price'
-            ]
+            'total_price',
+            'store_item_data',
+            'is_active',
+            'created_at',
+            'updated_at'
+        ]
+        read_only_fields = [
+            'is_active',
+            'created_at',
+            'updated_at'
+        ]
         
-        
+    def create(self, validated_data):
+        user = self.context['request'].user
+        store_item = validated_data.pop['store_item']
+        quantity = validated_data.pop['quantity']
+        cart, _ = Cart.objects.get_or_create(user=user)
+        cart_item, created = CartItem.objects.get_or_create(
+            cart=cart, store_item=store_item
+        )
+        if not created:
+            cart_item.quantity += quantity
+        else:
+            cart_item.quantity = quantity
+        cart_item.save()
+        return cart_item  
+    
+    
 class CartSerializer(serializers.ModelSerializer):
     total_price = serializers.FloatField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
     
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'total_price', 'items']
-        
+        fields = [
+            'id',
+            'user',
+            'total_price',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'items'
+        ]
+        read_only_fields = [
+            'is_active',
+            'created_at',
+            'updated_at'
+        ]
