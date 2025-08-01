d175adee3a2ec6ce8906acf948465f72b3527ffd
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-01 14:04:43 +0330

    add some methods for calculating price and total_price in OrderItem class model and total_price in  Order class model.

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 from base.models import BaseModel
+from orders.utils import safe_float
 from locations.models import Address
 from stores.models import StoreItem
 
@@ -38,6 +39,18 @@ class Order(BaseModel):
         max_length=100, blank=True, null=True
         )
     
+    @property
+    def get_total_price(self):
+        total = 0
+        for item in self.items.active():
+            total += safe_float(item.total_price)
+        return str(total)
+        
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.total_price = self.get_total_price
+        return super().save(*args, **kwargs)      
+        
     def __str__(self):
         return f"""
                 Order {self.id} by {self.customer.phone}/
@@ -63,6 +76,23 @@ class OrderItem(BaseModel):
         max_length=100, blank=True, null=True
         )
 
+    @property
+    def get_price(self):
+        if self.store_item.discount_price:
+            return self.store_item.discount_price
+        return self.store_item.price
+    
+    @property
+    def get_total_price(self):
+        if self.price and self.quantity > 0:
+            return str(safe_float(self.price) * self.quantity)
+        return "0.0"
+    
+    def save(self, *args, **kwargs):
+        self.price = self.get_price
+        self.total_price = self.get_total_price
+        return super().save(*args, **kwargs)
+        
     def __str__(self):
         return f"""
                 Item {self.id} in Order 
