19b3685c376dabde93ab313d4f6bb9f7d2c3608b
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-01 14:00:56 +0330

    add CartItemListAPIView , AcrtItemDetailAPIView , CartListAPIView and CartDetailAPIView classes with inheritance from GenericAPIView in carts.views.py and change url pathes in carts/urls.py

--- backend/carts/urls.py
+++ backend/carts/urls.py
@@ -3,14 +3,12 @@ from django.urls import path
 from carts.views import (
     CartItemListAPIView,
     CartItemDetailAPIView,
-    CartListAPIView,
     CartDetailAPIView
 )
 
 urlpatterns = [
-    path('cart/', CartListAPIView.as_view(), name='cart-list'),
-    path('cart/<int:id>/', CartDetailAPIView.as_view(), name='cart-detail'),
-    path('cart_item/', CartItemListAPIView.as_view(), name='cart-item-list'),
-    path('cart_item/<int:id>/', CartItemDetailAPIView.as_view(), name='cart-item-detail'),
+    path('cart/', CartDetailAPIView.as_view(), name='cart-detail'),
+    path('items/', CartItemListAPIView.as_view(), name='cart-item-list'),
+    path('items/<int:id>/', CartItemDetailAPIView.as_view(), name='cart-item-detail'),
     
 ]
--- backend/carts/views.py
+++ backend/carts/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from base.permissions import IsOwnerObject
@@ -16,66 +13,22 @@ from carts.models import (
     Cart
 )
 
-
-class CartListAPIView(GenericAPIView):
-    serializer_class = CartSerializer
-    authentication_classes = [JWTAuthentication]
-    
-    def get_queryset(self):
-        return Cart.objects.filter(user=self.request.user).active()
-    
-    def get(self, request):
-        carts = self.get_queryset()
-        serializer = self.get_serializer(carts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        if user != self.request.user.id:
-            raise PermissionDenied("Can't create cart you don't own")
-        serializer.save(user=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class CartDetailAPIView(GenericAPIView):
+class CartDetailAPIView(generics.RetrieveDestroyAPIView):
     serializer_class = CartSerializer
     permission_classes = [IsOwnerObject]
     authentication_classes = [JWTAuthentication]
     
-    def get_object(self, id):
-        obj = get_object_or_404(Cart, id=id)
+    def get_object(self):
+        obj, _ = Cart.objects.get_or_create(user=self.request.user)
         self.check_object_permissions(self.request, obj)
         return obj
-    
-    def get(self, request, id):
-        serializer = self.get_serializer(instance=self.get_object(id))
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def put(self, request, id):
-        serializer = self.get_serializer(
-            instance=self.get_object(id), data=request.data
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def patch(self, request, id):
-        serializer = self.get_serializer(
-            instance=self.get_object(id), data=request.data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def delete(self, request, id):
-        cart = self.get_object(id)
-        cart.soft_delete()
-        return Response({"deleted": True}, status=status.HTTP_204_NO_CONTENT)
+    def perform_destroy(self, instance):
+        instance.soft_delete()
+        instance.items.active().soft_delete()
     
 
-class CartItemListAPIView(GenericAPIView):
+class CartItemListAPIView(generics.ListCreateAPIView):
     serializer_class = CartItemSerializer
     permission_classes = [IsOwnerOfCartItem]
     authentication_classes = [JWTAuthentication]
@@ -83,58 +36,21 @@ class CartItemListAPIView(GenericAPIView):
     def get_queryset(self):
         return CartItem.objects.filter(cart__user=self.request.user).active()
     
-    def get(self, request):
-        car_items = self.get_queryset()
-        serializer = self.get_serializer(car_items, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        cart = Cart.objects.filter(user=request.user).active().first()
-        if not cart:
-            return Response(
-                {"detail": "Active cart not found."}, status=status.HTTP_400_BAD_REQUEST
-            )
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        cart_data = serializer.validated_data['cart']
-        if cart_data != cart.id:
-            raise PermissionDenied("Can't create cart-item you don't own")
+    def perform_create(self, serializer):
+        cart, _ = Cart.objects.get_or_create(
+            user = self.request.user
+        )
         serializer.save(cart=cart)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class CartItemDetailAPIView(GenericAPIView):
+        
+        
+class CartItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CartItemSerializer
     permission_classes = [IsOwnerOfCartItem]
     authentication_classes = [JWTAuthentication]
     
-    def get_object(self, id):
-        obj =get_object_or_404(CartItem, id=id)
-        self.check_object_permissions(self.request, obj)
-        return obj
-    
-    def get(self, request, id):
-        serializer = self.get_serializer(instance=self.get_object(id))
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def put(self, request, id):
-        serializer = self.get_serializer(
-            instance=self.get_object(id), data=request.data
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def patch(self, request, id):
-        serializer = self.get_serializer(
-            instance=self.get_object(id), data=request.data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user).active()
 
-    def delete(self, request, id):
-        cart_item = self.get_object(id)
-        cart_item.soft_delete()
-        return Response({"deleted": True}, status=status.HTTP_204_NO_CONTENT)
+    def perform_destroy(self, instance):
+        instance.soft_delete()
         
