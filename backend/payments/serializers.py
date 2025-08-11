6c9c435f1e7ca1bf2977a8a7e67289a98c0a7129
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-11 14:32:26 +0330

    refactoring fields and read_only_fields in payments/serializers.py PaymentSerializer class.

--- backend/payments/serializers.py
+++ backend/payments/serializers.py
@@ -7,5 +7,31 @@ class PeymentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Payment
-        fields = '__all__'
-        read_only_fields = '__all__'
+        fields = [
+            'id',
+            'order',
+            'status',
+            'transaction_id',
+            'amount',
+            'reference_id',
+            'card_pan',
+            'fee',
+            'payment_date',
+            'updated_at',
+            'created_at',
+            'is_active'
+        ]
+        read_only_fields = [
+            'id',
+            'order',
+            'status',
+            'transaction_id',
+            'amount',
+            'reference_id',
+            'card_pan',
+            'fee',
+            'payment_date',
+            'updated_at',
+            'created_at',
+            'is_active'
+        ]
