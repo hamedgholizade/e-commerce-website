81e2c4e781a55b97f2896f477fd312a55d951c1a
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-15 10:13:21 +0330

    fix: add name to payment URL and optimize queryset in PaymentListAPIView

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from payments.views import PaymentListAPIView
 
+app_name='payments'
+
 
 urlpatterns = [
-    path('payment/', PaymentListAPIView.as_view())
+    path('payment/', PaymentListAPIView.as_view(), name='payment-list')
 ]
--- backend/payments/views.py
+++ backend/payments/views.py
@@ -15,4 +15,4 @@ class PaymentListAPIView(ListAPIView):
     def get_queryset(self):
         return self.queryset.filter(
             order__customer=self.request.user
-            )
+            ).select_related('order')
