81ccbb026606a952d652ca7e21c28be741fa248c
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-15 09:34:50 +0330

    refactor: enhance Cart and CartItem models with total_discount calculation and improve unit_price handling

--- backend/carts/models.py
+++ backend/carts/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, InvalidOperation
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -21,6 +22,22 @@ class Cart(BaseModel):
     @property
     def total_price(self):
         return sum(item.total_price for item in self.items.active())
+    
+    @property
+    def total_discount(self):
+        total_disc = 0
+        for item in self.items.active():
+            if hasattr(item.store_item, "discount_price"):
+                try:
+                    price = Decimal(str(item.store_item.price))
+                    disc_price = Decimal(str(item.store_item.discount_price))
+                    if disc_price < price:
+                        total_disc += (price - disc_price) * item.quantity
+                except (InvalidOperation, TypeError):
+                    continue
+            else:
+                continue
+        return total_disc
 
     def __str__(self):
         return f"Cart of {self.user.phone}"
@@ -45,13 +62,16 @@ class CartItem(BaseModel):
 
     @property
     def unit_price(self):
-        if self.store_item.discount_price:
-            return self.store_item.discount_price
-        return self.store_item.price
+        price = self.store_item.discount_price or self.store_item.price
+        try:
+            return Decimal(str(price))
+        except (InvalidOperation, TypeError):
+            return Decimal("0")
+        
     
     @property
     def total_price(self):
-        return float(self.unit_price) * self.quantity
+        return self.unit_price * self.quantity
     
     def __str__(self):
         return f"{self.store_item.product.name} in {self.cart.user.phone}'s cart"
