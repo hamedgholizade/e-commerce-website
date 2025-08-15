d184f135042718ac19117b734b7d5aa16447821b
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-15 09:43:00 +0330

    refactor: enhance permission checks in ReviewModelViewSet for create, update, and destroy actions.

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import PermissionDenied
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters import rest_framework as dj_filters
 
@@ -16,5 +17,18 @@ class ReviewModelViewSet(ModelViewSet):
     filter_backends = [dj_filters.DjangoFilterBackend]
     filterset_class = ReviewFilter
     
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    def perform_update(self, serializer):
+        user = self.request.user
+        if not user.is_staff or not user.is_superuser:
+            if serializer.instance.user.id != user.id:
+                raise PermissionDenied("Can't update comment you aren't owner.")
+        serializer.save()
+    
     def perform_destroy(self, instance):
+        user = self.request.user
+        if not (user.is_staff or user.is_superuser or instance.user.id == user.id):
+            raise PermissionDenied("Can't delete comment you aren't owner.")
         instance.soft_delete()
