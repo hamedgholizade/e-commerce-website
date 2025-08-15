7f16afcc6ba2505d524e0889dce2debbce5eff62
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-15 09:43:10 +0330

    fix: add app_name to reviews URL configuration for namespacing

--- backend/reviews/urls.py
+++ backend/reviews/urls.py
@@ -2,6 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from reviews.views import ReviewModelViewSet
 
+app_name='reviews'
 
 router = DefaultRouter()
 router.register('review', ReviewModelViewSet)
