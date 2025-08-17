ea32a6b518de07d6e773ed394e8248aa079cef4f
hamedgholizade <hamedabbasgholi33@gmail.com>
2025-08-17 12:06:49 +0330

    add app_name var in locations/urls.py for accessing url pathes of this file.

--- backend/locations/urls.py
+++ backend/locations/urls.py
@@ -2,6 +2,8 @@ from rest_framework.routers import DefaultRouter
 
 from locations.views import AddressModelViewSet
 
+app_name='locations'
+
 router = DefaultRouter()
 router.register('address', AddressModelViewSet)
 
